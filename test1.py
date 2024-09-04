5e37184d5e5b1756dca10fe1310c3c22e7e717c8
mashmool0 <ap4games2017@gmail.com>
2024-09-04 14:19:27 +0330

    first commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+sex = input('enter your sex : ')  ## man  , woman
+name = input('enter your name : ')
+
+
+
+if sex == "man" : 
+    print('aghaye ',name)  
+if sex == "man" : 
+    print("khanoom",name)
+
+elif sex == 'non-binary' : 
+    print("aziz",name)
+
+else : 
+    print("somthing went wrong !! please enter your sex again")
